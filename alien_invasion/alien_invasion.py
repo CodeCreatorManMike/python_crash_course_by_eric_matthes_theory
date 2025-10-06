6d1cac744a47ab2966c14dcc5fbd0a52eaef217a
CodeCreatorManMike <michaeljonesincorporated@gmail.com>
2025-10-06 13:53:26 +0100

    moving all files into a folder

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,44 @@
+import pygame
+import sys
+
+
+
+from settings import Settings
+from ship import Ship
+
+
+class AlienInvasion:
+    """overall class to manage assets/behaviour"""
+
+    def __init__(self):
+        """Initialize the game, create resources"""
+        pygame.init()
+        self.settings = Settings()
+    
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+
+        pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
+    
+    
+    def run_game(self):
+        "main game loop"
+        while True:
+            # watch for keyboard events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # redraw the screen during each pass through the loop
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme
+
+            # make the most recently drawn screen available
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    # make a game instance, run the game
+    ai = AlienInvasion()
+    ai.run_game()
---
+++ alien_invasion/settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """A class which stores all of the settings"""
+
+    def __init__(self):
+        "initialize game settings"
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
